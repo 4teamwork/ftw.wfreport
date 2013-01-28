59b13fe97f7f039a6bdc89c4ccb17daef0e2c928
Jonas Baumann <jone@jone.ch>
2013-01-28 11:23:36 +0100

    Update classifiers.

--- setup.py
+++ setup.py
@@ -18,9 +18,14 @@ setup(name='ftw.wfreport',
           open(os.path.join("docs", "HISTORY.txt")).read(),
 
       # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
+
       classifiers=[
-        "Programming Language :: Python",
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.1',
+        'Framework :: Plone :: 4.2',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         ],
 
       keywords='plone workflow report ftw',
